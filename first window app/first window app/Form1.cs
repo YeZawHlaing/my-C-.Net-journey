6425bdfc76fa5fd4fd240c9325ee2275d64b8b10
YeZawHlaing <yezawhlaing0@gmail.com>
2024-12-05 23:02:43 +0630

    initial commit

---
+++ first window app/first window app/Form1.cs
@@ -0,0 +1,15 @@
+namespace first_window_app
+{
+    public partial class Frmcalc : Form
+    {
+        public Frmcalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ my C# journey/my C# journey/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Helloworld()
+{
+    static void Main(String[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+
+    }
+}
+
+
+
+
