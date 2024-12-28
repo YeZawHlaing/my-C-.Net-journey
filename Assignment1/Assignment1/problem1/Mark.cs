af9c174c831635b34373b07f25b23157406199ed
YeZawHlaing <yezawhlaing0@gmail.com>
2024-12-28 12:06:55 +0630

    conditional statement

---
+++ Assignment1/Assignment1/problem1/Mark.cs
@@ -0,0 +1,49 @@
+﻿
+//using System;
+//public class Mark
+//{
+//    public static void Main(string[] args)
+//    {
+        
+//        Console.WriteLine("Enter your first subject mark");
+//        int mark1 =int.Parse(Console.ReadLine());
+//      //  Console.WriteLine("your mark is"+mark1);
+
+
+//        Console.WriteLine("Enter your second subject mark");
+//        int mark2 = int.Parse(Console.ReadLine());
+//      //  Console.WriteLine("your mark is" + mark2);
+
+
+//        Console.WriteLine("Enter your third subject mark");
+//        int mark3 = int.Parse(Console.ReadLine());
+//        //Console.WriteLine("your mark is" + mark3);
+
+
+//        if (mark1 < 50 || mark2 < 50 || mark3 < 50)
+//        {
+//            Console.WriteLine("fail");
+//        }
+      
+//        else if((mark1 >= 50 && mark1<=64) && (mark2>=50 && mark2<=64) && (mark3 >= 50 && mark3<=64))
+//        {
+//            Console.WriteLine("pass");
+//        }
+//        else if ((mark1 >= 65 && mark1 <= 84) && (mark2 >= 65 && mark2 <= 84) && (mark3 >= 65 && mark3 <= 84))
+//        {
+//            Console.WriteLine("credit");
+//        }
+//        else if ((mark1 >= 85 && mark1 <= 100) || (mark2 >= 85 && mark2 <= 100) || (mark3 >= 85 && mark3 <= 100))
+//        {
+//            Console.WriteLine("Di");
+//        }
+
+//        else
+//        {
+//            Console.WriteLine("pass");
+//        }
+
+//    }
+
+
+//}
---
+++ Assignment1/Assignment1/problem2/Star.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Star
+{
+    public static void Main(string[] args)
+    {
+        int row = 5;
+        for (int i = 1; i <= row; i++)
+        {
+
+            for (int j = 1; j <= row-i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
